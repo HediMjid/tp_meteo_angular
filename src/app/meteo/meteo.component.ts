a7ee7c58d87ccd46195d86ee90eef3608bf51c5b
unknown <hedi.mjid@gmail.com>
2021-11-21 18:16:11 +0100

    tableau js

--- src/app/meteo/meteo.component.ts
+++ src/app/meteo/meteo.component.ts
@@ -20,6 +20,13 @@ export class MeteoComponent implements OnInit {
     .then(responsFormat => {
       this.dataMeteoResp=responsFormat;
       console.log(this.dataMeteoResp);
+      var i = 0;
+      var jour_suivant = new Array();
+      for(i=0;i<=4;i++){
+        var lejour = "fcst_day_"+i;
+        jour_suivant.push({"day_date":responsFormat[lejour]['date'],"day_long":responsFormat[lejour]['day_long'],"day_icon":responsFormat[lejour]['icon_big'],"day_tmax":responsFormat[lejour]['tmin'],"day_tmin":responsFormat[lejour]['tmax']});
+      }
+      console.log(jour_suivant);
     })
     .catch(err => {
         console.error(err);
