853607e186907ce7446967b991631c36eb72cea1
HediMjid <hedi.mjid@gmail.com>
2021-11-21 15:31:38 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MeteoComponent } from './meteo/meteo.component';
+import { SearchComponent } from './search/search.component';
+import { MeteoJourComponent } from './meteo-jour/meteo-jour.component';
+import { MeteoSuivantComponent } from './meteo-suivant/meteo-suivant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeteoComponent,
+    SearchComponent,
+    MeteoJourComponent,
+    MeteoSuivantComponent
   ],
   imports: [
     BrowserModule
