20fc6ab5a82eb0230da143c0d61f1f0c27be8329
HediMjid <hedi.mjid@gmail.com>
2021-11-24 11:07:28 +0100

    commentaire

--- src/app/meteo/meteo.component.ts
+++ src/app/meteo/meteo.component.ts
@@ -23,17 +23,24 @@ export class MeteoComponent implements OnInit {
         return response.json()
     })
     .then(responsFormat => {
+      // on recreer nous meme un tableau avec les infos necessaires pour plus tard
+      //initiation des variables
+      // i pour la boucle
       var i = 0;
+      // declaration d'un tableau pour lui push les infos necessaires aux jours suivants
       var jour_suivant = new Array();
+      // declaration et remplissage du tableau avec les infos de la journée
       var jour_j = {"day_city":responsFormat['city_info']['name'],"day_date":responsFormat['current_condition']['date'],"day_hour":responsFormat['current_condition']['hour'],"day_icon":responsFormat['current_condition']['icon_big'],"day_tmp":responsFormat['current_condition']['tmp']};
       //this.dataMeteoResp=responsFormat;
       //console.log(responsFormat);
       
+      // boucle pour rechercher les infos des jours suivants, qu'on push dans notre tableau jour_suivants
       for(i=0;i<=4;i++){
         var lejour = "fcst_day_"+i;
         jour_suivant.push({"day_date":responsFormat[lejour]['date'],"day_long":responsFormat[lejour]['day_long'],"day_icon":responsFormat[lejour]['icon_big'],"day_tmax":responsFormat[lejour]['tmax'],"day_tmin":responsFormat[lejour]['tmin']});
       }
 
+      // on ajoute nos 2 tableau dans un seul, que l'on passe dans dataMeteoResp
       this.dataMeteoResp={"jour_j":jour_j,"jour_suivants":jour_suivant};
       console.log(this.dataMeteoResp);
     })
